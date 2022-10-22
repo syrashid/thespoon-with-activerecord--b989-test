5ff06c660ef3e4b19435d4e5ce7af3ac8cb523f3
Sy Rashid <sy.rashid.11@gmail.com>
2022-10-22 15:27:12 +0200

    FEAT: Add validations for restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # See all restaurants
+  # See details about one restaurant
+  # Create a restaurant
+  # Update a restaurant
+  # Destroy a restaurant
 end

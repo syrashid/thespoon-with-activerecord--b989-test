1da90a9e2822fc12274c06efef19903d3fdf0187
Sy Rashid <sy.rashid.11@gmail.com>
2022-10-22 22:04:58 +0200

    FEAT: Add index feature for restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # See all restaurants
+  get "restaurants", to: "restaurants#index"
   # See details about one restaurant
+  get "restaurants/:id", to: "restaurants#show"
   # Create a restaurant
   # Update a restaurant
   # Destroy a restaurant
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Clearing DB"
+Restaurant.destroy_all
+
+puts "Generating Restaurants"
+100.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, rating: rand(1..5))
+end
+
+puts "#{Restaurant.count} restaurants created"

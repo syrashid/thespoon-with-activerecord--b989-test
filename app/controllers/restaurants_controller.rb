c5bfa87f056b23586532892cc27ec46567453304
Sy Rashid <sy.rashid.11@gmail.com>
2022-10-22 22:53:01 +0200

    FEAT: Finish coding all actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,40 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # No need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    # No need for app/views/restaurants/update.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # See all restaurants
   get "restaurants", to: "restaurants#index"
-  # See details about one restaurant
-  get "restaurants/:id", to: "restaurants#show"
   # Create a restaurant
+  get "restaurants/new", to: "restaurants#new"
+  # See details about one restaurant
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  post "restaurants", to: "restaurants#create"
   # Update a restaurant
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
   # Destroy a restaurant
+  delete "restaurants/:id", to: "restaurants#destroy"
 end
